0ccd3cedf07d1696c44ad847333233de18bc1c1c
Enigma010 <jjodarsk@yahoo.com>
2019-04-17 19:48:05 -0500

    Adding infrastructure to support testing without the distance sensor

--- App/Config/Config.js
+++ App/Config/Config.js
@@ -1,7 +1,8 @@
 const _ = require('lodash');
 
 module.exports = class Config{
-    constructor(webListenOnPort, triggerPinNum, echoPinNum){
+    constructor(webListenOnPort, triggerPinNum, echoPinNum, maxDistance, minDistance, maxHistoryLength, 
+        testRefreshMinutes, testFunction){
         if(_.isUndefined(webListenOnPort) || _.isNull(webListenOnPort)){
             webListenOnPort = 3000;
         }
@@ -11,12 +12,37 @@ module.exports = class Config{
         if(_.isUndefined(echoPinNum) || _.isNull(echoPinNum)){
             echoPinNum = 24;
         }
+        if(_.isUndefined(testRefreshMinutes) || _.isNull(testRefreshMinutes)){
+            testRefreshMinutes = 1;
+        }
+        if(_.isUndefined(testFunction) || typeof testFunction !== 'function'){
+            testFunction = function(){
+                this.SetCurrentDistance(Math.random() * this.Config.MaxDistance);
+            }
+        }
+        if(_.isUndefined(maxDistance) || _.isNull(maxDistance)){
+            maxDistance = 2900;
+        }
+        if(_.isUndefined(minDistance) || _.isNull(minDistance)){
+            minDistance = 0;
+        }
+        if(_.isUndefined(maxHistoryLength) || _.isNull(maxHistoryLength)){
+            maxHistoryLength = 3;
+        }
+
         this.Web = {
             ListenOnPort: webListenOnPort
         };
         this.DistanceSensor = {
             TriggerPinNum: triggerPinNum,
-            EchoPinNum: echoPinNum
+            EchoPinNum: echoPinNum,
+            MaxDistance: maxDistance,
+            MinDistance: minDistance,
+            MaxHistoryLength: maxHistoryLength,
+            Test: {
+                RefreshMinutes: testRefreshMinutes,
+                TestFunction: testFunction
+            }
         };
     }
 }
--- App/Model/DistanceSensor.js
+++ App/Model/DistanceSensor.js
@@ -1,22 +1,54 @@
-const Gpio = require('pigpio').Gpio;
-
+if(typeof process.env.NODE_ENV !== 'undefined' && process.env.NODE_ENV !== 'development'){
+    const Gpio = require('pigpio').Gpio;
+}
+const _ = require('lodash');
 
 module.exports = class DistanceSensor{
     constructor(config){
         this.Config = config;
-        this.Trigger = new Gpio(this.Config.TriggerPinNum, {mode: Gpio.OUTPUT});
-        this.Echo = new Gpio(this.Config.EchoPinNum, {mode: Gpio.INPUT, alert: true});
+        if(typeof Gpio !== 'undefined'){
+            this.Trigger = new Gpio(this.Config.TriggerPinNum, {mode: Gpio.OUTPUT});
+            this.Echo = new Gpio(this.Config.EchoPinNum, {mode: Gpio.INPUT, alert: true});
+        }
         this.MICROSECDONDS_PER_CM = 1e6/34321;
+        this.DistanceHistory = [];
     }
 
     Start(){
-        this.Trigger.digitalWrite(0); // Make sure trigger is low
-        this.HandleTriggerFunc = _.bind(this.HandleTrigger, this);
-        this.HandleTriggerFunc();
-        // Trigger a distance measurement once per second
-        setInterval(_.bind(function(){
-            this.Trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-        }, this), 1000);
+        if(typeof Gpio !== 'undefined'){
+            this.Trigger.digitalWrite(0); // Make sure trigger is low
+            this.HandleTriggerFunc = _.bind(this.HandleTrigger, this);
+            this.HandleTriggerFunc();
+            // Trigger a distance measurement once per second
+            setInterval(_.bind(function(){
+                this.Trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+            }, this), 1000);
+        }
+        else{
+            setInterval(_.bind(this.Config.Test.TestFunction, this), this.Config.Test.RefreshMinutes * 60 * 1000);
+        }
+    }
+
+    SetCurrentDistance(distance){
+        if(this.DistanceHistory.length > this.Config.MaxHistoryLength){
+            this.DistanceHistory.pop();
+        }
+        this.DistanceHistory.push(distance);
+
+        if(this.IsValidDistance(distance)){
+            this.CurrentDistance = distance;
+            return;
+        }
+
+        _.forEach(this.DistanceHistory, _.bind(function(checkDistance){
+            if(this.IsValidDistance(checkDistance)){
+                this.CurrentDistance = checkDistance;
+            }
+        }, this));
+    }
+
+    IsValidDistance(distance){
+        return distance >= this.Config.MinDistance && distance <= this.Config.MaxDistance
     }
 
     HandleTrigger(level, tick){
@@ -25,7 +57,7 @@ module.exports = class DistanceSensor{
           } else {
             const endTick = tick;
             const diff = (endTick >> 0) - (this.StartTick >> 0); // Unsigned 32 bit arithmetic
-            this.CurrentDistance = diff / 2 / this.MICROSECDONDS_PER_CM;
+            this.SetCurrentDistance(diff / 2 / this.MICROSECDONDS_PER_CM);
           }      
     }
 }
